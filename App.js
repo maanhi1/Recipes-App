94ffd4247b54234cf643695c91e5b318358367cf
maanhi1 <maanhiiiii@gmail.com>
2024-06-11 09:32:21 +0700

    add first code

---
+++ App.js
@@ -0,0 +1,22 @@
+import HomeScreen from "./src/screens/HomeScreen";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Recipe App</Text>
+      <StatusBar style="auto" />
+      {/* <HomeScreen></HomeScreen> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
