828f7b50d84e0cca241dced915f2728fd0e316ac
maanhi1 <maanhiiiii@gmail.com>
2024-06-18 14:11:12 +0700

    add connect server & add recipe screen

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import HomeScreen from "./src/screens/HomeScreen";
 import SignupScreen from "./src/screens/SignupScreen";
 import BottomNavigation from "./src/components/BottomNavigation";
+import Vidu from "./src/screens/vidu";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
@@ -8,7 +9,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <SignupScreen></SignupScreen>
+      {/* <SignupScreen></SignupScreen> */}
+      <BottomNavigation />
     </View>
   );
 }
